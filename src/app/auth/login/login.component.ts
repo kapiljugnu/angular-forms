4b51dd5489d734bdd8c05f34625f109de6aac666
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-11 11:58:10 +0530

    added validation attributes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,6 +10,10 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+    
     const email = formData.form.value.email;
     const password = formData.form.value.password;
   }
