404f670e284a8884fb3ee0c175612e9025b0415c
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-13 12:33:21 +0530

    associate reactive form with form in template

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,14 @@
 import {
   Component,
 } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule]
 })
 export class LoginComponent {
   form = new FormGroup({
