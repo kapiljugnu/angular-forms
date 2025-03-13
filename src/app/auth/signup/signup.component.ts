62843791edef9e8a464ac6f67dd663642a8a5d7f
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-13 13:34:24 +0530

    setup reactive form for signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule]
 })
-export class SignupComponent {}
+export class SignupComponent {
+  singUpForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+  })
+
+  onReset() {
+    this.singUpForm.reset()
+  }
+
+  onSubmit() {
+    console.log(this.singUpForm.value.email)
+  }
+}
