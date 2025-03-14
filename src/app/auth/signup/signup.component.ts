7c9500df99655dff4b403c54b280059c89427859
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-14 15:18:32 +0530

    custom validation for multiple field

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,52 +1,86 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+// function equalValues(control: AbstractControl) {
+//   const password = control.get('password')?.value;
+//   const confirmPassword = control.get('confirmPassword')?.value;
+//   if (password === confirmPassword) {
+//     return null;
+//   }
+//   return { passwordNotEqual: true };
+// }
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) =>  {
+    const password = control.get(controlName1)?.value;
+    const confirmPassword = control.get(controlName2)?.value;
+    if (password === confirmPassword) {
+      return null;
+    }
+    return { passwordNotEqual: true };
+  }
+}
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
-  imports: [ReactiveFormsModule]
+  imports: [ReactiveFormsModule],
 })
 export class SignupComponent {
   singUpForm = new FormGroup({
     email: new FormControl('', {
-      validators: [Validators.email, Validators.required]
+      validators: [Validators.email, Validators.required],
     }),
-    passwords: new FormGroup({
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)]
-      }),
-      confirmPassword: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)]
-      }),
+    passwords: new FormGroup(
+      {
+        password: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+        confirmPassword: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+      },
+      {
+        validators: [equalValues('password', 'confirmPassword')],
+      }
+    ),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
     }),
-    firstName: new FormControl('', {validators: [Validators.required]}),
-    lastName: new FormControl('', {validators: [Validators.required]}),
-    address:  new FormGroup({
-      street: new FormControl('', {validators: [Validators.required]}),
-      number: new FormControl('', {validators: [Validators.required]}),
-      postalCode: new FormControl('', {validators: [Validators.required]}),
-      city: new FormControl('', {validators: [Validators.required]}),
-    }),
-    role: new FormControl<'student'|'teacher'|'employee'|'founder'|'other'>('student', {validators: [Validators.required]}),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [Validators.required] }),
     source: new FormArray([
       new FormControl(false),
       new FormControl(false),
       new FormControl(false),
     ]),
-    agree: new FormControl(false, {validators:[Validators.required]})
-  })
+    agree: new FormControl(false, { validators: [Validators.required] }),
+  });
 
   onReset() {
-    this.singUpForm.reset()
+    this.singUpForm.reset();
   }
 
   onSubmit() {
-    if (this.singUpForm.invalid){
+    if (this.singUpForm.invalid) {
       console.log('Invalid Form');
       return;
     }
-    console.log(this.singUpForm)
+    console.log(this.singUpForm);
   }
 }
